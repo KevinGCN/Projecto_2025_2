306c33c63a7a63d57a108cc9f06d3b60bef28510
Jhoan1316 <jhoandiegom@gmail.com>
2025-11-27 17:40:33 -0500

    NUeva clase (reducir nodo)

---
+++ Guerra_Poderes/src/Clases/MotorBatalla.java
@@ -0,0 +1,29 @@
+package Clases;
+
+public class MotorBatalla {
+
+    public static void atacar(Carta atacante, Carta objetivo) {
+
+        int ataque = atacante.getAtaque();
+        double daño;
+
+        // Relación elemental
+        if (atacante.getElemento().equals(objetivo.getDebilidad())) {
+            daño = ataque * 1.2;
+        } else if (atacante.getDebilidad().equals(objetivo.getElemento())) {
+            daño = ataque * 0.7;
+        } else {
+            daño = ataque;
+        }
+
+        // Aplicar daño
+        objetivo.setVida(objetivo.getVida() - (int) daño);
+
+        // Consumir energía
+        atacante.setEnergia(atacante.getEnergia() - 1);
+
+        if (objetivo.getVida() <= 0) {
+            System.out.println(objetivo.getNombre() + " ha sido derrotado.");
+        }
+    }
+}
